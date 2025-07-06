16be27c50f5d2952a56dd9cc1a7d42402a59e506
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2025-07-06 22:54:03 +0530

    added search methods in maps

--- Hashmaps/hashtable.cpp
+++ Hashmaps/hashtable.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include<map>
+#include<unordered_map>
 using namespace std;
 
 int main() {
-    
+    // creation 
+    unordered_map <string , int> m;
+    // Insertion methods
+    // Method 1
+    pair<string,int>p = make_pair("Abhigyan",1);
+    m.insert(p);
+    // Method 2
+    pair<string,int> pair2("Aniket",17);
+    m.insert(pair2);
+    // Method 3 
+    m["Anupama"] = 46;
+
+
+
+    // Search
+    cout<<m["Abhigyan"]<<endl;
+    cout<<m.at("Aniket")<<endl;
     return 0;
 }
