1bbac77f3c01593ccf7ad2f5c0a10cc06b84bcc8
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2025-07-22 23:16:52 +0530

    updated dtor and its exe file

--- OOPs/destructor.cpp
+++ OOPs/destructor.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 class Student{
 public:
-    int id ;
+    int id ; // every variable till nos is stored in a stack memory
     int age;
     string name;
     int nos;
+    int *v; // a new pointer is made
     // constructor
     Student(int id , int age , string name , int nos){
         this->id= id;
         this->age= age;
         this->name= name;
         this->nos= nos ;
+        this->v= new int(2); // this will be stored in a heap memory ,  if not freed then memory will leak 
+        // hence a destructor is needed
     }
     // Copy Constructor 
     Student(const Student &srcobj){
@@ -22,8 +25,10 @@ public:
         this->nos = srcobj.nos;
 
     }
+    // destructor
     ~Student(){
         cout<<"dtor called"<<endl;
+        delete v;
     }
 };
 int main() {
