62dea988cee001c0a14e7a0c3f94e7a3cfeef8cd
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2025-07-22 14:29:37 +0530

    added how array and pointers work together

--- pointers/pointer.cpp
+++ pointers/pointer.cpp
@@ -8,5 +8,11 @@ int main() {
   int arr[5] = {1,2,3,4,5};
   cout<<"the size of array in the main function is "<<sizeof(arr)<<endl;
   solve(arr,5);
+  // array of pointers 
+  int *a[5] ;
+
+  // Pointer to an array - two methods
+  int *ptr = arr;
+  int (*ptr)[5] = &arr;
   return 0;
 }
