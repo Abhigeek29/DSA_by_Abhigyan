6ed744dde245dd2ac9f20c006e1a862e40a9a233
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2026-01-01 12:23:16 +0530

    added one new method for making graphs

--- Graph/basic.cpp
+++ Graph/basic.cpp
@@ -2,29 +2,50 @@
 #include<vector>
 using namespace std;
 
-int main() {
-    vector<pair<int,int>> edges = {
-        {0,1},
-        {1,2},
-        {1,3},
-        {2,3}
-    };
-    int n = edges.size();
-    // initialised adjacency matrix
-    vector<vector<int>> adjacencymatrix(n,vector<int>(n,0));
-    // filling the matrix
-    for(auto it :edges){
-        int u = it.first;
-        int v = it.second;
-        adjacencymatrix[u][v] = 1 ; 
-        // adjacencymatrix[v][u] = 1 ;  // in case of undirected graphs
-    }
-    //printing graph
+void print(int n , vector<vector<int>>&matrix){
     for(int i = 0 ; i<n;i++){
-        for(int j = 0 ; j<n;j++){
-            cout<<adjacencymatrix[i][j]<<" ";
+        for(int j = 0 ; j<n ; j++){
+            cout<<matrix[i][j]<<" ";
         }
         cout<<endl;
     }
-    return 0;
+}
+// int main(){
+//     vector<pair<int,int>> edges = {
+//         {0,1},
+//         {0,2},
+//         {1,2},
+//         {1,3},
+//         {2,4},
+//         {3,4},
+//     };
+//     int n ; //
+//     cin>>n; 
+//     vector<vector<int>> matrix(n ,vector<int>(n,0)); // adjacency matrix initialized to 0
+//     // now traverse and place 
+//     for(auto it : edges){
+//         // it holds the pair 
+//         int u = it.first;
+//         int v = it.second;
+//         matrix[u][v] = 1;
+//         // matrix[v][u] = 1; // for undirected graph
+//     }
+//     print(n,matrix);
+//     return 0;
+// }
+
+int main(){
+    int vertices , edges ;
+    cout<<"Please first enter no. of vertices and then edges"<<endl;
+    cin>> vertices>>edges;
+
+    vector<vector<int>> adjacencymatrix(vertices, vector<int>(edges,0)); // 0 init
+    int u , v;
+    for(int i = 0 ; i<edges;i++){
+        cin>>u>>v;
+        adjacencymatrix[u][v] = 1;
+        adjacencymatrix[v][u] = 1;
+    }
+    print(vertices,adjacencymatrix);
+    return 0 ;
 }
