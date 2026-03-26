e9c138a7b353157115f9989da1601009d4d5f6b4
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2026-03-26 12:50:15 +0530

    added method 1 and 2 using recursion

--- Recursion/pws.cpp
+++ Recursion/pws.cpp
@@ -4,27 +4,53 @@
 #include <string.h>
 #include <string>
 using namespace std;
-void rec(string &s,string&output,int index){
-    if(index>=s.length()){
+// method 1
+// void rec(string &s,string&output,int index){
+//     if(index>=s.length()){
+//         cout<<output<<" ";
+//         return;
+//     }
+//     // without space 
+//     output.push_back(s[index]);
+//     rec(s,output,index+1);
+//     output.pop_back();
+//     // with space
+//     output.push_back('_');
+//     output.push_back(s[index]);
+//     rec(s,output,index+1);
+//     output.pop_back();
+//     output.pop_back();
+
+// }
+// int main() {
+//     string s = "abcd";
+//     string output;
+//     output.push_back(s[0]);
+//     rec(s,output,1);
+//     return 0;
+// }
+// method 2 
+void rec(string s , string output){
+    if(s.size()==0){
         cout<<output<<" ";
         return;
     }
+    char charnew = s[0];
+    string rest = s.substr(1);
+
     // without space 
-    output.push_back(s[index]);
-    rec(s,output,index+1);
-    output.pop_back();
-    // with space
-    output.push_back('_');
-    output.push_back(s[index]);
-    rec(s,output,index+1);
-    output.pop_back();
-    output.pop_back();
+    rec(rest, output + charnew);
+    // with space 
+    rec(rest,output + '_' + charnew);
 
 }
-int main() {
-    string s = "abcd";
+int main(){
+    string s = "abc";
     string output;
     output.push_back(s[0]);
-    rec(s,output,1);
+    rec(s.substr(1),output);
     return 0;
 }
+
+
+
