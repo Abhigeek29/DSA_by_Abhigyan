9b71f0b730e423bd24d282da13a273a538d39a53
Abhigyan Sahoo <Sahooabhigyan24@gmail.com>
2026-04-04 20:00:33 +0530

    top down approach bhi likhdia

--- Dynamic Programming/adityaverma/knapsack.cpp
+++ Dynamic Programming/adityaverma/knapsack.cpp
@@ -3,6 +3,28 @@
 #include <vector>
 using namespace std;
 vector<vector<int>> t(1002,vector<int>(1002,-1));
+int tabu(vector<int>&wt,vector<int>&val,int capacity,int n){
+    for(int i = 0 ; i<n+1;i++){
+        for(int j = 0  ; j<capacity+1;j++){
+            if(i==0 ||j==0){
+                t[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 1; j < capacity + 1; j++) {
+            if (wt[i - 1] <= j) {
+                t[i][j] = max(
+                    val[i - 1] + t[i - 1][j - wt[i - 1]],
+                    t[i - 1][j]
+                );
+            } else {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+    return t[n][capacity];
+}
 int knapsack(vector<int>&wt,vector<int>&val,int capacity,int n){
     if(n==0 || capacity==0){
         return 0;
@@ -34,6 +56,7 @@ int main() {
     int capacity = 7;
     int ans = knapsack(wt,val,capacity,wt.size());
     int answer = memo(wt,val,capacity,wt.size());
-    cout<<ans<<" "<<answer;
+    int answers = tabu(wt,val,capacity,wt.size());
+    cout<<ans<<" "<<answer<<" "<<answers;
     return 0;
 }
